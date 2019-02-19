86a60414b234bd944311c7baef4750c9067b2e66
smdsbz <694066681@qq.com>
2019-02-19 19:45:21 +0800

    updating InstructionWordSpliter.v

--- src/design/InstructionWordSpliter.v
+++ src/design/InstructionWordSpliter.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Author:      Xiaoguang Zhu
-// Version:     2.18 16:11
+// Version:     2.19 19:44
 // Reviewer:
 // Review Date:
 //////////////////////////////////////////////////////////////////////////////////
@@ -17,7 +17,8 @@ module InstructionWordSpliter
     output  wire    [4:0]       rd,         // destination register
     output  wire    [4:0]       shamt,      // misc parameter to ~ALU~
     output  wire    [5:0]       funct,      // function to ~controller~
-    output  wire    [15:0]      immediate   // immediate number
+    output  wire    [15:0]      immediate,  // immediate number
+    output  wire    [25:0]      instr_idx   // for jump `J`
 );
 
 assign opcode   = instr_word[31:26];
@@ -27,5 +28,6 @@ assign rd       = instr_word[15:11];
 assign shamt    = instr_word[10:6];
 assign funct    = instr_word[5:0];
 assign immediate = instr_word[15:0];
+assign instr_idx = instr_word[25:0];
 
 endmodule
