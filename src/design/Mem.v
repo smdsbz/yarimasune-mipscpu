cf63e26678fba0ad5afa08b9a6e7042f6c0808c1
aha222 <15608652022@163.com>
2019-02-19 23:20:37 +0800

    Update Mem.v

--- src/design/Mem.v
+++ src/design/Mem.v
@@ -14,32 +14,38 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Mem #(parameter MEM_ADDR_BITS=20,parameter MEM_DATA_BITS=32)(addr, data_in, str, sel, clk, ld, clr, data_out);
+module Mem #(parameter MEM_ADDR_BITS=10,parameter MEM_DATA_BITS=32)(addr, data_in, str, sel, clk, ld, clr, data_out);
     input [MEM_ADDR_BITS-1:0]addr;
     input [MEM_DATA_BITS-1:0]data_in;
     input str,clk,ld,clr;
     input [3:0]sel;
     output reg [MEM_DATA_BITS-1:0]data_out;
     reg [MEM_DATA_BITS-1:0]memory[0:(1<<MEM_ADDR_BITS)-1];
-    reg [31:0]data_in_;
+    wire [31:0]data_in_;
     reg [31:0]data_reg;
     wire [31:0]sel_2;
     integer i;
     assign sel_2={{8{sel[3]}},{8{sel[2]}},{8{sel[1]}},{8{sel[0]}}};
+    assign data_in_={(sel[3]!=0)?data_in[31:24]:memory[addr][31:24],
+                     (sel[2]!=0)?data_in[23:16]:memory[addr][23:16],
+                     (sel[1]!=0)?data_in[15:8]:memory[addr][15:8],
+                     (sel[0]!=0)?data_in[7:0]:memory[addr][7:0]};
+    
+    initial
+        begin
+            for(i=0;i<(1<<MEM_ADDR_BITS);i=i+1)
+                memory[i]=32'h0;
+        end
     always @(posedge clk)
         begin
              if(clr)
                 begin
                     for(i=0;i<(1<<MEM_ADDR_BITS);i=i+1)
-                        memory[i]=32'h0;
-                    data_out=32'd0;
+                        memory[i]<=32'h0;
+                    data_out<=32'd0;
                 end//清零
              else
                 begin
-                    data_in_={(sel[3]!=0)?data_in[31:24]:memory[addr][31:24],
-                            (sel[2]!=0)?data_in[23:16]:memory[addr][23:16],
-                            (sel[1]!=0)?data_in[15:8]:memory[addr][15:8],
-                            (sel[0]!=0)?data_in[7:0]:memory[addr][7:0]};
                     if(str)
                         memory[addr]=data_in_;
                     else;
