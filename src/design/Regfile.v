828702d2eea81ff6e110b5a7089c14fed6e2f266
Alexcyh7 <504492693@qq.com>
2019-02-19 15:47:06 +0800

    Add files via upload
    Update Regfile, add three new output (cyh)

--- src/design/Regfile.v
+++ src/design/Regfile.v
@@ -1,52 +1,41 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2019/02/18 15:53:11
-// Design Name: 
-// Module Name: Regfile
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Author:  Yuhang Chen
+// Version: 2.19 10.50
+//
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module Regfile(
-    IR1,IR2,W,Din,WE,CLK,RST,OR1,OR2
+    IR1,IR2,W,Din,WE,CLK,RST,OR1,OR2,a0,v0,ra
     );
-    input wire [4:0] IR1;
-    input wire [4:0] IR2;
-    input wire [4:0] W;
-    input wire [31:0] Din;
-    input wire WE;
+    input wire [4:0] IR1;   //Rs寄存器�?�择信号
+    input wire [4:0] IR2;   //Rt寄存器�?�择信号
+    input wire [4:0] W;     //写寄存器选择信号
+    input wire [31:0] Din;  //写入数据
+    input wire WE;          //写使能，�?1时写入数�?
     input wire CLK;
     input wire RST;
-    output wire [31:0] OR1;
-    output wire [31:0] OR2;
-    reg [31:0] REG [0:31];
+    output wire [31:0] OR1; //输出R1
+    output wire [31:0] OR2; //输出R2
+    output wire [31:0] a0;  //寄存器a0(4号寄存器)
+    output wire [31:0] v0;  //寄存器v0(2号寄存器)
+    output wire [31:0] ra;  //寄存器ra(31号寄存器)
+    reg [31:0] REG [0:31];  //寄存器堆
+
     assign OR1 = REG[IR1];
     assign OR2 = REG[IR2];
-    //�����ش���
+    assign a0 = REG[4];
+    assign v0 = REG[2];
+    assign ra = REG[31];
+
     always @(posedge CLK) begin
-        if(RST) begin
-            REG[0] <= 0;
-            //reg����
-        end
-        else begin
-            if(WE & (W != 0)) begin
-                REG[W] <= Din;
-            end
+        REG[0] = 0;
+        if(WE & (W != 0)) begin
+            REG[W] <= Din;
         end
         //OR1 <= REG[IR1];
-        //OR2 <= REG[IR2];  
+        //OR2 <= REG[IR2];
     end
 endmodule
