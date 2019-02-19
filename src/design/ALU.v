6862b22509a32c993a1dbb64315cd99f147cc855
zhangjiaxinghust <33047126+zhangjiaxinghust@users.noreply.github.com>
2019-02-19 09:22:59 +0800

    Update and rename ALU to ALU.v

--- src/design/ALU.v
+++ src/design/ALU.v
@@ -1,34 +1,41 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Author:      JiaXing Zhang
-// Version:     2.19 8.39
+// Version:     2.19 9:21
 // Reviewer:
 // Review Date:
 //////////////////////////////////////////////////////////////////////////////////
 
 // 
 // ALU
-module ALU(ALU_OP,X,Y,Result,Result2 );
 
-parameter digit_number=32; 
+module ALU(ALU_OP,X,Y,shamt,Result,Result2,equal,overflow);
+
+parameter digit_number=32; //data length
 input [3:0] ALU_OP;
-input [digit_number-1:0] X;
-input [digit_number-1:0] Y;
+input [digit_number-1:0] X;//operating nuber1
+input [digit_number-1:0] Y;//operating nuber2
+input [4:0] shamt;
 output reg [digit_number-1:0] Result;
 output reg [digit_number-1:0] Result2;
+output  equal;
+output  overflow;
+
+assign equal=((X==Y)? 1:0);
+assign overflow=0;
 always @(*)
 begin
 	case(ALU_OP)
 	4'b0000: begin
-		Result=X<<Y[4:0];
+		Result=X<<shamt;
 		Result2=0;
 		end
     4'b0001: begin
-		Result=X>>>Y[4:0];
+		Result=X>>>shamt;
 		Result2=0;
 		end
     4'b0010: begin
-		Result=X>>Y[4:0];
+		Result=X>>shamt;
 		Result2=0;
 		end
     4'b0011: {Result2,Result}=X*Y;
