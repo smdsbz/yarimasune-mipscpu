fefd55c6ae5e96b9a88655c9c06bad679d303f7d
smdsbz <694066681@qq.com>
2019-02-21 00:24:25 +0800

    modifying LHSpecialRegisters, valid on ready and posedge clk

--- src/design/LHSpecialRegisters.v
+++ src/design/LHSpecialRegisters.v
@@ -1,19 +1,20 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Author:      Xiaoguang Zhu
-// Version:     2.19 11:26
+// Version:     2.21 8:15
 // Reviewer:
 // Review Date:
 //////////////////////////////////////////////////////////////////////////////////
 
 // LO / HI special registers as individual multiplier / divider output
 //
-// LHSpecialRegisters stores @result into @lo and @hi when a rising edge is given
+// LHSpecialRegisters stores @result into @lo and @hi on @ready at @clk rising edge
 // on @ready.
 module LHSpecialRegisters
 #(
     parameter   DATA_BITS   = 32
 )(
+    input   wire                            clk,
     input   wire    [DATA_BITS * 2 - 1:0]   result,     // calculated result
     input   wire                            ready,      // is `result` ready ?
     output  wire    [DATA_BITS - 1:0]       lo,
@@ -22,8 +23,10 @@ module LHSpecialRegisters
 
 reg     [DATA_BITS * 2 - 1:0]   hi_lo;
 
-always @ (posedge ready) begin
-    hi_lo <= result;
+always @ (posedge clk) begin
+    if (ready) begin
+        hi_lo <= result;
+    end
 end
 
 assign  lo = hi_lo[DATA_BITS - 1:0];
--- src/simulation/Sim_LHSpecialRegisters.v
+++ src/simulation/Sim_LHSpecialRegisters.v
@@ -25,10 +25,12 @@ module Sim_LHSpecialRegisters (
     output  wire    [31:0]  hi
 );
 
+reg             clk;
 reg     [63:0]  result;
 reg             ready;
 
 initial begin
+    #0      clk = 0;
     #0      ready = 0;
     #0      result = 0;
     // floating result, should have no effets on lo and hi
@@ -36,11 +38,14 @@ initial begin
     // give ready, lo, hi should be refreshed, result should be hold
     #5      ready = 1;
     // retrieve ready
-    #5      ready = 0;
+    #13     ready = 0;
     #5      result = 0;
 end
 
+always @ * #3 clk <= ~clk;
+
 LHSpecialRegisters LHSpecialRegisters_TestMod (
+    .clk(clk),
     .result(result),
     .ready(ready),
     .lo(lo),
